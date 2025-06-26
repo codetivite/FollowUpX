202abd07091c5f8ad067e36787cece25e741e6e5
Aloydeachieve <donaloy777@gmail.com>
2025-06-26 23:47:01 +0100

    feat: improve footer layout and content

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -8,14 +8,14 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.footerLinks}>
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>Integration</li>
-          <li>About</li>
-          <li>Careers</li>
-          <li>Contact</li>
-          <li>Privacy</li>
-          <li>Terms</li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#pricing">Pricing</a></li>
+          <li><a href="/integrations">Integration</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="">Careers</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/privacy">Privacy</a></li>
+          <li><a href="">Terms</a></li>
         </div>
         <div className={styles.footerLogo}>
           <Image 
